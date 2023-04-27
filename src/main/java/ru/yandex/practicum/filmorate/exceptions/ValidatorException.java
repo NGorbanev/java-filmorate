4bb4b9424e51455f2fbf53e392c572385c734ab7
Nikita <nikitosiusss@yandex.ru>
2023-04-28 02:24:08 +0300

    update: codestyle fixed again

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidatorException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidatorException.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.logging.Logger;
